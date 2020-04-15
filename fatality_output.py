6d6c349fd1d02a724f6a16934e947aab83a94b36
unknown <paulmburu034@gmail.com>
2020-04-15 11:05:45 +0300

    Upgrade withith while loop

--- fatality_output.py
+++ fatality_output.py
@@ -1,20 +1,18 @@
 from case_fatality_rate import *
+import sys
 
 
 def main():
     print('Welcome  Choose any of the following to proceed')
-    print('1 : Mortality rate')
-    print('2 : Recovery rate')
-    print('3 : Active cases rate')
+    print("________________________________\nPress 1---> Mortality Rate\nPress 2---> Recovery Rate\nPress 3---> Active Cases Rate\nPress 4---> Exit\n________________________________")
+    
 
-    entry = int(input('Reply with any of the above choices: '))
-    start = 1
     case_fatality = Case_Fatality_Rate()
-    while start == 1:
+    while(1):
+        entry = int(input('Reply with any of the above choices: '))
         if entry == 1:
             death_rate = case_fatality.case_fatality_rate()
             print('The death rate for Covid-19 is: %0.2f' % death_rate, '%')
-            break
         elif entry == 2:
             recovery_rate = case_fatality.recovery_rate()
             print('The recovery rate for Covid 19 is: %0.2f' %
@@ -23,9 +21,11 @@ def main():
             current_cases = case_fatality.current_cases_rate()
             print('The Current active case for Covid-19 is: %0.2f' %
                   current_cases, '%')
+        elif entry == 4:
+            print("Exiting...")
+            sys.exit(0)
         else:
             print('Invalid choice try again')
-            start = 0
 
 
 print(main())
